02c0e095f0ef8e0e62d4f6742e9173f2216e5f9e
Андрей Черняев <ChernyaevAV@gmail.com>
2023-09-27 00:24:36 +0300

    feat: добавил автодокументирование swagger(drf-spectacular)

--- eatpoint/eatpoint/settings.py
+++ eatpoint/eatpoint/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'EatPoint API',
+    'DESCRIPTION': 'Документация для API сервиса',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
