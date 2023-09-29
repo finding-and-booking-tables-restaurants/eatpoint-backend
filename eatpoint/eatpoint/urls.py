58d98f3c60ff843337db375d12d6489734f49728
Rashid Ayupov <valentaine1234@gmail.com>
2023-09-29 15:31:52 +0300

    Отображение и добавление заведения.fix

--- eatpoint/eatpoint/urls.py
+++ eatpoint/eatpoint/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
-                                   SpectacularSwaggerView)
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    # Optional UI:
     path(
         "swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
