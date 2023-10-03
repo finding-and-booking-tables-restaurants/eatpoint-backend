ca12f935ce0b58b380ec07b53ca7f589fe243a32
Rashid Ayupov <valentaine1234@gmail.com>
2023-10-03 15:05:40 +0300

    Исправил настройки

--- eatpoint/eatpoint/settings.py
+++ eatpoint/eatpoint/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "establishments.apps.EstablishmentsConfig",
-    "rest_framework",
     "jwt",
     "api.apps.ApiConfig",
     "users.apps.UsersConfig",
@@ -138,6 +137,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
