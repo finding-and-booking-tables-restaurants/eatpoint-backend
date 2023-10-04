746a6bd6d923417c532574c751a329ffaeebd0ce
Rashid Ayupov <valentaine1234@gmail.com>
2023-10-05 01:22:20 +0300

    Модель телефона в админке

--- eatpoint/establishments/admin.py
+++ eatpoint/establishments/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django import forms
 from django.utils.safestring import mark_safe
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 from .models import Establishment, Kitchen, Table, Service, File, Work
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        widgets = {
+            "telephone": PhoneNumberPrefixWidget(initial="RU"),
+        }
 
 
 @admin.register(Work)
@@ -48,12 +57,12 @@ class FileInLine(admin.TabularInline):
 
 @admin.register(Establishment)
 class EstablishmentAdmin(admin.ModelAdmin):
+    form = ContactForm
     list_display = (
         "name",
         "id",
         "preview",
         "email",
-        "telephone",
         "is_verified",
     )
     list_filter = ("name",)
