16c7a49bbf6ac224ebee0c470e489409ad389a13
Андрей Черняев <ChernyaevAV@gmail.com>
2023-10-11 00:25:15 +0300

    feat: добавил фильтр заведений по тегам "Тип" + "Сервис"

--- eatpoint/api/filters/establishments.py
+++ eatpoint/api/filters/establishments.py
@@ -11,3 +11,27 @@ class EstablishmentFilterBackend(filters.BaseFilterBackend):
                 kitchens__slug__regex=regular_tags
             )
         return review_queryset.distinct()
+
+
+class TypeEstFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        establishment_types = request.query_params.getlist("types")
+        review_queryset = queryset
+        if establishment_types:
+            regular_tags = "|".join(establishment_types)
+            review_queryset = review_queryset.filter(
+                types__slug__regex=regular_tags
+            )
+        return review_queryset.distinct()
+
+
+class ServicesEstFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        establishment_services = request.query_params.getlist("services")
+        review_queryset = queryset
+        if establishment_services:
+            regular_tags = "|".join(establishment_services)
+            review_queryset = review_queryset.filter(
+                services__slug__regex=regular_tags
+            )
+        return review_queryset.distinct()
