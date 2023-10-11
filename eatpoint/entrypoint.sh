7d022d4f6dc82f281e59f578b7b58f6a5ea813d3
Rashid Ayupov <valentaine1234@gmail.com>
2023-10-11 14:04:04 +0300

    schema static

--- eatpoint/entrypoint.sh
+++ eatpoint/entrypoint.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 python3 manage.py migrate --no-input
-python3 ./manage.py spectacular --color --file schema.yml
 python3 manage.py collectstatic --no-input
 gunicorn eatpoint.wsgi:application --bind 0.0.0.0:8000
