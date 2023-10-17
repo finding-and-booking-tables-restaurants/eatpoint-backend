35b9adfc3f008706378e6ae6987932e78a5b5f90
Rashid Ayupov <valentaine1234@gmail.com>
2023-10-18 01:11:43 +0300

    docker utf-8 FIX

--- eatpoint/entrypoint.sh
+++ eatpoint/entrypoint.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
 python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
-python3 manage.py loaddata data/cities2.json
+python3 manage.py loaddata data/fixtures.json
 gunicorn eatpoint.wsgi:application --bind 0.0.0.0:8000
