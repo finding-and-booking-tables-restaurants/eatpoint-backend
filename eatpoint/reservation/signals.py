6b159ecb61c0b606f8e28a5375a1061a01b93ead
Rashid Ayupov <valentaine1234@gmail.com>
2023-10-17 23:25:21 +0300

    docker

--- eatpoint/reservation/signals.py
+++ eatpoint/reservation/signals.py
@@ -1,6 +1,6 @@
 from rest_framework.validators import ValidationError
-from reservation.models import Reservation
-from django.db.models.signals import post_save, pre_delete
+from reservation.models import Reservation, ReservationHistory
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
 
@@ -27,29 +27,22 @@ def delete_reservation(sender, instance, **kwargs):
         zone.available_seats = zone.seats
 
 
-# @receiver(post_save, sender=Reservation)
-# def move_booking_to_history(sender, instance, **kwargs):
-#     """Добавляет бронирование в историю"""
-#     now = timezone.now()
-#     end_reservation = datetime.strptime(instance.end_time_reservation, "%H:%M").time()
-#     res_end = datetime.combine(instance.date_reservation, end_reservation)
-#     print(now)
-#     print(res_end)
-#     if res_end < now:
-#         print('aboba')
-#         ReservationHistory.objects.create(
-#             user=instance.user,
-#             first_name = instance.first_name,
-#             last_name = instance.last_name,
-#             email = instance.email,
-#             telephone = instance.telephone,
-#             establishment = instance.establishment,
-#             zone = instance.zone,
-#             number_guests = instance.number_guests,
-#             date_reservation = instance.date_reservation,
-#             start_time_reservation = instance.start_time_reservation,
-#             end_time_reservation = instance.end_time_reservation,
-#             comment = instance.comment,
-#             status=False,
-#         )
-#         instance.delete()
+@receiver(post_delete, sender=Reservation)
+def move_booking_to_history(sender, instance, **kwargs):
+    """Добавляет бронирование в историю"""
+    ReservationHistory.objects.create(
+        user=instance.user,
+        first_name=instance.first_name,
+        last_name=instance.last_name,
+        email=instance.email,
+        telephone=instance.telephone,
+        establishment=instance.establishment,
+        zone=instance.zone,
+        number_guests=instance.number_guests,
+        date_reservation=instance.date_reservation,
+        start_time_reservation=instance.start_time_reservation,
+        end_time_reservation=instance.end_time_reservation,
+        comment=instance.comment,
+        status=False,
+    )
+    instance.delete()
