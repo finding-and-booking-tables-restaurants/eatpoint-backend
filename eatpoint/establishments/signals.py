53cf4772084bfb24cb72fd6c15b4bcb275338ec6
Rashid Ayupov <valentaine1234@gmail.com>
2023-11-01 23:31:33 +0300

    Zone fix

--- eatpoint/establishments/signals.py
+++ eatpoint/establishments/signals.py
@@ -4,6 +4,7 @@ import locale
 from establishments.models import (
     WorkEstablishment,
     ZoneEstablishment,
+    Establishment,
 )
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -11,10 +12,8 @@ from django.dispatch import receiver
 from reservation.models import Availability
 
 
-@receiver(post_save, sender=WorkEstablishment)
-def create_availability_work(sender, instance, created, **kwargs):
+def __create(establishment):
     locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
-    establishment = instance.establishment
     start_date = datetime.now().date()
     days = 7
     zones = ZoneEstablishment.objects.filter(establishment=establishment)
@@ -33,27 +32,21 @@ def create_availability_work(sender, instance, created, **kwargs):
                     available_seats=zone.seats,
                     establishment=establishment,
                 )
+
+
+@receiver(post_save, sender=WorkEstablishment)
+def create_availability_work(sender, instance, created, **kwargs):
+    establishment = instance.establishment
+    __create(establishment)
 
 
 @receiver(post_save, sender=ZoneEstablishment)
 def create_availability_zone(sender, instance, created, **kwargs):
-    locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
     establishment = instance.establishment
-    start_date = datetime.now().date()
-    days = 7
-    zones = ZoneEstablishment.objects.filter(establishment=establishment)
-    for day in range(days):
-        current_date = start_date + timedelta(days=day)
-        day_of_week = current_date.strftime("%A").lower()
-        if WorkEstablishment.objects.filter(
-            establishment=establishment,
-            day=day_of_week,
-            day_off=False,
-        ).exists():
-            for zone in zones:
-                Availability.objects.get_or_create(
-                    zone=zone,
-                    date=current_date,
-                    available_seats=zone.seats,
-                    establishment=establishment,
-                )
+    __create(establishment)
+
+
+@receiver(post_save, sender=Establishment)
+def create_availability_est(sender, instance, created, **kwargs):
+    establishment = instance
+    __create(establishment)
