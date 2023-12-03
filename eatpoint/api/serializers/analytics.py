41833dd5cb8b02b09012e60b60429f4b63585214
Yuriy <info@juriys.ru>
2023-12-03 22:38:55 +0300

    add days

--- eatpoint/api/serializers/analytics.py
+++ eatpoint/api/serializers/analytics.py
@@ -1,10 +1,6 @@
 from rest_framework import serializers
-from django.db.models import Count
 
 from django.utils.timezone import now
-
-from establishments.models import Establishment
-from reservation.models import Reservation
 
 
 class AnalyticsStaticSerializer(serializers.Serializer):
@@ -19,6 +15,14 @@ class AnalyticsStaticSerializer(serializers.Serializer):
     )
     weekly_reservation = serializers.IntegerField(
         help_text="Бронирования за неделю",
+    )
+    monthly_reservation = serializers.DictField(
+        child=serializers.IntegerField(),
+        help_text="Бронирования по месяцам",
+    )
+    daily_reservations_last_week = serializers.DictField(
+        child=serializers.IntegerField(),
+        help_text="Бронирования по дням",
     )
     yearly_reservation = serializers.IntegerField(
         help_text="Бронирования за год",
@@ -41,17 +45,16 @@ class AnalyticsStaticSerializer(serializers.Serializer):
             "total_reservation": instance["total_reservation"],
             "daily_reservation": instance["daily_reservation"],
             "weekly_reservation": instance["weekly_reservation"],
+            "daily_reservations_last_week": instance[
+                "daily_reservations_last_week"
+            ],
+            "monthly_reservation": instance["monthly_reservation"],
             "yearly_reservation": instance["yearly_reservation"],
         }
 
 
 class AnalyticsDynamicSerializer(serializers.Serializer):
-    """
-    Сериализатор для аналитики в выбранном временном периоде.
-
-    Предоставляет возможность получения аналитических данных о бронированиях
-    для указанного заведения в указанном временном диапазоне.
-    """
+    """Аналитика зы выбранные период"""
 
     start_date = serializers.DateTimeField(
         format="%Y-%m-%d %H:%M",
@@ -61,64 +64,28 @@ class AnalyticsDynamicSerializer(serializers.Serializer):
     )
     end_date = serializers.DateTimeField(
         format="%Y-%m-%d %H:%M",
+        default=now(),
         write_only=True,
         help_text="Конец отслеживания",
     )
     total_reservation = serializers.IntegerField(read_only=True)
+    daily_reservations_last_week = serializers.ListField(
+        child=serializers.DictField(), read_only=True
+    )
+    daily_reservations = serializers.ListField(
+        child=serializers.DictField(), read_only=True
+    )
+    monthly_reservations = serializers.ListField(
+        child=serializers.DictField(), read_only=True
+    )
 
     def to_representation(self, instance):
-        """
-        Преобразует объект сериализатора в словарь с данными аналитики.
-
-        Args:
-            instance: Экземпляр сериализатора.
-
-        Returns:
-            dict: Словарь с данными аналитики.
-
-        """
-        return {
-            "total_reservation": instance["total_reservation"],
+        representation = {
+            "total_reservation": instance.get("total_reservation"),
+            "daily_reservations_last_week": instance.get(
+                "daily_reservations_last_week"
+            ),
+            "daily_reservations": instance.get("daily_reservations"),
+            "monthly_reservations": instance.get("monthly_reservations"),
         }
-
-    def get_reservation_analytics(
-        self, start_date, end_date, establishment_id
-    ):
-        """
-        Получает данные аналитики по бронированиям для указанного заведения в указанном временном диапазоне.
-
-        Args:
-            start_date (datetime.datetime): Начальная дата отслеживания бронирований.
-            end_date (datetime.datetime): Конечная дата отслеживания бронирований.
-            establishment_id (int): ID заведения для получения аналитики.
-
-        Returns:
-            int: Общее количество бронирований за указанный период времени.
-
-        Raises:
-            serializers.ValidationError: Если указанное заведение не существует.
-        """
-        end_date = end_date or now()
-
-        try:
-            establishment = Establishment.objects.get(id=establishment_id)
-        except Establishment.DoesNotExist:
-            raise serializers.ValidationError(
-                "Указанное заведение не существует"
-            )
-
-        reservations = (
-            Reservation.objects.filter(
-                establishment=establishment,
-                date_reservation__range=(start_date, end_date),
-            )
-            .values("date_reservation__date")
-            .annotate(reservation_count=Count("id"))
-        )
-
-        analytics = {
-            res["date_reservation__date"]: res["reservation_count"]
-            for res in reservations
-        }
-
-        return sum(analytics.values())
+        return representation
--- eatpoint/api/views/analytics.py
+++ eatpoint/api/views/analytics.py
@@ -1,9 +1,14 @@
+from datetime import timedelta
+
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from django.utils.timezone import now
+from django.db.models import Count
+from django.db.models.functions import ExtractMonth
+
 
 from api.permissions import IsRestorateur
 from api.serializers.analytics import (
@@ -53,6 +58,29 @@ class AnalyticsViewSet(APIView):
             establishment=establishment_id,
             reservation_date__week=now().isocalendar()[1],
         ).count()
+
+        monthly_reservation = (
+            ReservationHistory.objects.filter(
+                establishment=establishment_id,
+                reservation_date__year=now().year,
+            )
+            .annotate(month=ExtractMonth("reservation_date"))
+            .values("month")
+            .annotate(monthly_count=Count("id"))
+        )
+
+        daily_reservations_last_week = (
+            ReservationHistory.objects.filter(
+                establishment=establishment_id,
+                reservation_date__date__range=[
+                    now().date() - timedelta(days=6),
+                    now().date() + timedelta(days=1),
+                ],
+            )
+            .values("reservation_date__date")
+            .annotate(reservation_count=Count("id"))
+        )
+
         yearly_reservation = ReservationHistory.objects.filter(
             establishment=establishment_id, reservation_date__year=now().year
         ).count()
@@ -61,6 +89,8 @@ class AnalyticsViewSet(APIView):
             "total_reservation": total_reservation,
             "daily_reservation": daily_reservation,
             "weekly_reservation": weekly_reservation,
+            "monthly_reservation": list(monthly_reservation),
+            "daily_reservations_last_week": list(daily_reservations_last_week),
             "yearly_reservation": yearly_reservation,
         }
         serializer = AnalyticsStaticSerializer(aggregated_data)
@@ -70,37 +100,124 @@ class AnalyticsViewSet(APIView):
         responses=AnalyticsDynamicSerializer,
         request=AnalyticsDynamicSerializer,
     )
+    # def post(self, request, establishment_id):
+    #     serializer = AnalyticsDynamicSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         start_date = serializer.validated_data["start_date"]
+    #         end_date = serializer.validated_data["end_date"]
+    #
+    #         total_reservation = ReservationHistory.objects.filter(
+    #             establishment=establishment_id,
+    #             reservation_date__range=[start_date, end_date],
+    #         ).count()
+    #         reservations_by_day = ReservationHistory.objects.filter(
+    #             establishment=establishment_id,
+    #             reservation_date__range=[start_date, end_date],
+    #         ).values('reservation_date__date').annotate(reservation_count=Count('id'))
+    #
+    #         # Преобразовать QuerySet в список словарей для сериализации
+    #         reservations_by_day_list = list(reservations_by_day)
+    #
+    #         aggregated_data = {
+    #             "total_reservation": total_reservation,
+    #             "reservations_by_day": reservations_by_day_list,
+    #             # Можете добавить другие данные здесь, если необходимо
+    #         }
+    #
+    #         serializer = AnalyticsDynamicSerializer(aggregated_data)
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     def post(self, request, establishment_id):
+        """
+        Получение динамической аналитики по POST запросу.
+
+        :param request: Объект запроса REST API.
+        :return: Данные динамической аналитики или сообщение об ошибке.
+        """
+        user = self.request.user
         serializer = AnalyticsDynamicSerializer(data=request.data)
         if serializer.is_valid():
             start_date = serializer.validated_data["start_date"]
             end_date = serializer.validated_data["end_date"]
+
+            filters = {"establishment__owner": user}
+            if start_date and end_date:
+                filters["reservation_date__range"] = [start_date, end_date]
+
             total_reservation = ReservationHistory.objects.filter(
-                establishment=establishment_id,
-                reservation_date__range=[start_date, end_date],
+                **filters
             ).count()
-            daily_reservation = ReservationHistory.objects.filter(
-                establishment=establishment_id,
-                reservation_date__date=now().date(),
-            ).count()
-            weekly_reservation = ReservationHistory.objects.filter(
-                establishment=establishment_id,
-                reservation_date__week=now().isocalendar()[1],
-            ).count()
-            yearly_reservation = ReservationHistory.objects.filter(
-                establishment=establishment_id,
-                reservation_date__year=now().year,
-            ).count()
+
+            daily_reservations = (
+                ReservationHistory.objects.filter(
+                    establishment=establishment_id,
+                    reservation_date__date=start_date,
+                )
+                .values("reservation_date__date")
+                .annotate(reservation_count=Count("id"))
+            )
+            daily_reservations_list = list(daily_reservations)
+            if not daily_reservations_list:
+                daily_reservations_list.append(
+                    {
+                        "reservation_date__date": start_date,
+                        "reservation_count": 0,
+                    }
+                )
+
+            daily_reservations_last_week = (
+                ReservationHistory.objects.filter(
+                    establishment=establishment_id,
+                    reservation_date__range=[start_date, end_date],
+                )
+                .values("reservation_date__date")
+                .annotate(reservation_count=Count("id"))
+            )
+            daily_reservations_last_week_list = list(
+                daily_reservations_last_week
+            )
+
+            monthly_reservations = (
+                ReservationHistory.objects.filter(
+                    establishment=establishment_id, **filters
+                )
+                .annotate(month=ExtractMonth("reservation_date"))
+                .values("month")
+                .annotate(monthly_count=Count("id"))
+            )
+            monthly_reservations_list = [
+                {
+                    "month": entry["month"],
+                    "monthly_count": entry["monthly_count"],
+                }
+                for entry in monthly_reservations
+            ]
+            monthly_reservations_list = [
+                entry
+                for entry in monthly_reservations_list
+                if entry["monthly_count"] > 0
+            ]
+            monthly_reservations_list = sorted(
+                monthly_reservations_list, key=lambda x: x["month"]
+            )
 
             aggregated_data = {
                 "total_reservation": total_reservation,
-                "daily_reservation": daily_reservation,
-                "weekly_reservation": weekly_reservation,
-                "yearly_reservation": yearly_reservation,
+                "daily_reservations": daily_reservations_list,
+                "daily_reservations_last_week": daily_reservations_last_week_list,
+                "monthly_reservations": monthly_reservations_list,
             }
+
             serializer = AnalyticsDynamicSerializer(aggregated_data)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        else:
+            error_message = (
+                "Некорректные данные. Пожалуйста, проверьте запрос."
+            )
+            return Response(
+                {"error": error_message}, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 @extend_schema(
@@ -136,7 +253,53 @@ class AnalyticsListViewSet(APIView):
         :return: Данные статической аналитики.
         """
         user = self.request.user
-        aggregated_data = self._get_aggregated_data(user=user)
+        now_date = now().date()
+
+        filters = {"establishment__owner": user}
+        total_reservation = ReservationHistory.objects.filter(
+            **filters
+        ).count()
+
+        daily_reservation = ReservationHistory.objects.filter(
+            **filters, reservation_date__date=now_date
+        ).count()
+
+        weekly_reservation = ReservationHistory.objects.filter(
+            **filters, reservation_date__week=now().isocalendar()[1]
+        ).count()
+
+        monthly_reservation = (
+            ReservationHistory.objects.filter(**filters)
+            .annotate(month=ExtractMonth("reservation_date"))
+            .values("month")
+            .annotate(monthly_count=Count("id"))
+        )
+
+        daily_reservations_last_week = (
+            ReservationHistory.objects.filter(
+                **filters,
+                reservation_date__date__range=[
+                    now_date - timedelta(days=6),
+                    now_date + timedelta(days=1),
+                ],
+            )
+            .values("reservation_date__date")
+            .annotate(reservation_count=Count("id"))
+        )
+
+        yearly_reservation = ReservationHistory.objects.filter(
+            **filters, reservation_date__year=now().year
+        ).count()
+
+        aggregated_data = {
+            "total_reservation": total_reservation,
+            "daily_reservation": daily_reservation,
+            "weekly_reservation": weekly_reservation,
+            "daily_reservations_last_week": list(daily_reservations_last_week),
+            "monthly_reservation": list(monthly_reservation),
+            "yearly_reservation": yearly_reservation,
+        }
+
         serializer = AnalyticsStaticSerializer(aggregated_data)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -156,11 +319,74 @@ class AnalyticsListViewSet(APIView):
         if serializer.is_valid():
             start_date = serializer.validated_data["start_date"]
             end_date = serializer.validated_data["end_date"]
-            aggregated_data = self._get_aggregated_data(
-                user=user, start_date=start_date, end_date=end_date
+
+            filters = {"establishment__owner": user}
+            if start_date and end_date:
+                filters["reservation_date__range"] = [start_date, end_date]
+
+            total_reservation = ReservationHistory.objects.filter(
+                **filters
+            ).count()
+
+            daily_reservations = (
+                ReservationHistory.objects.filter(
+                    reservation_date__date=start_date
+                )
+                .values("reservation_date__date")
+                .annotate(reservation_count=Count("id"))
             )
+            daily_reservations_list = list(daily_reservations)
+            if not daily_reservations_list:
+                daily_reservations_list.append(
+                    {
+                        "reservation_date__date": start_date,
+                        "reservation_count": 0,
+                    }
+                )
+
+            daily_reservations_last_week = (
+                ReservationHistory.objects.filter(
+                    reservation_date__range=[start_date, end_date],
+                )
+                .values("reservation_date__date")
+                .annotate(reservation_count=Count("id"))
+            )
+            daily_reservations_last_week_list = list(
+                daily_reservations_last_week
+            )
+
+            monthly_reservations = (
+                ReservationHistory.objects.filter(**filters)
+                .annotate(month=ExtractMonth("reservation_date"))
+                .values("month")
+                .annotate(monthly_count=Count("id"))
+            )
+            monthly_reservations_list = [
+                {
+                    "month": entry["month"],
+                    "monthly_count": entry["monthly_count"],
+                }
+                for entry in monthly_reservations
+            ]
+            monthly_reservations_list = [
+                entry
+                for entry in monthly_reservations_list
+                if entry["monthly_count"] > 0
+            ]
+            monthly_reservations_list = sorted(
+                monthly_reservations_list, key=lambda x: x["month"]
+            )
+
+            aggregated_data = {
+                "total_reservation": total_reservation,
+                "daily_reservations": daily_reservations_list,
+                "daily_reservations_last_week": daily_reservations_last_week_list,
+                "monthly_reservations": monthly_reservations_list,
+            }
+
             serializer = AnalyticsDynamicSerializer(aggregated_data)
             return Response(serializer.data, status=status.HTTP_200_OK)
+
         else:
             error_message = (
                 "Некорректные данные. Пожалуйста, проверьте запрос."
@@ -168,36 +394,3 @@ class AnalyticsListViewSet(APIView):
             return Response(
                 {"error": error_message}, status=status.HTTP_400_BAD_REQUEST
             )
-
-    def _get_aggregated_data(self, user, start_date=None, end_date=None):
-        """
-        Получение агрегированных данных по бронированиям.
-
-        :param user: Пользователь для фильтрации данных.
-        :param start_date: Начальная дата для фильтрации (необязательно).
-        :param end_date: Конечная дата для фильтрации (необязательно).
-        :return: Словарь с агрегированными данными по бронированиям.
-        """
-        filters = {"establishment__owner": user}
-        if start_date and end_date:
-            filters["reservation_date__range"] = [start_date, end_date]
-
-        total_reservation = ReservationHistory.objects.filter(
-            **filters
-        ).count()
-        daily_reservation = ReservationHistory.objects.filter(
-            **filters, reservation_date__date=now().date()
-        ).count()
-        weekly_reservation = ReservationHistory.objects.filter(
-            **filters, reservation_date__week=now().isocalendar()[1]
-        ).count()
-        yearly_reservation = ReservationHistory.objects.filter(
-            **filters, reservation_date__year=now().year
-        ).count()
-
-        return {
-            "total_reservation": total_reservation,
-            "daily_reservation": daily_reservation,
-            "weekly_reservation": weekly_reservation,
-            "yearly_reservation": yearly_reservation,
-        }
