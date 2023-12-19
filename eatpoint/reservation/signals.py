ccf76b548f0124cb93a6af9ab25a660745d0cd5a
Андрей Черняев <ChernyaevAV@gmail.com>
2023-12-19 19:42:57 +0300

    feat: v2 новая модель Бронирования

--- eatpoint/reservation/signals.py
+++ eatpoint/reservation/signals.py
@@ -4,6 +4,39 @@ from django.dispatch import receiver
 
 
 @receiver(pre_save, sender=Reservation)
+def reservation_status_changed(sender, instance, **kwargs):
+    """Добавляет бронирование в историю"""
+    if instance._state.adding:
+        # Если это новая запись, то ничего не делаем
+        return None
+
+    old_reservation = Reservation.objects.get(id=instance.id)
+    slots = ",\n".join([str(slot) for slot in instance.slots.all()])
+
+    if (
+        old_reservation.is_visited != instance.is_visited
+        and instance.is_visited is True
+    ):
+        ReservationHistory.objects.create(
+            reservation_date=instance.reservation_date,
+            establishment=instance.establishment,
+            date_reservation=instance.date_reservation,
+            start_time_reservation=instance.start_time_reservation,
+            is_accepted=instance.is_accepted,
+            is_visited=instance.is_visited,
+            first_name=instance.first_name,
+            last_name=instance.last_name,
+            email=instance.email,
+            telephone=instance.telephone,
+            slots=slots,
+            comment=instance.comment,
+            reminder_one_day=instance.reminder_one_day,
+            reminder_three_hours=instance.reminder_three_hours,
+            reminder_half_on_hour=instance.reminder_half_on_hour,
+        )
+
+
+@receiver(pre_save, sender=Reservation)
 def post_reservations(sender, instance, **kwargs):
     """Уменьшает количество свободных мест, если появилась запись о бронировании"""
 
